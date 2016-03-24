bec5d0b461e3cae2a00a756078f2da383ae0588d
Jason Lemieux <jason@gopostmatic.com>
2016-03-24 09:14:39 +0000

    fixed i8n string

--- assets/templates/elevated-comment.php
+++ assets/templates/elevated-comment.php
@@ -8,7 +8,7 @@
  */
 ?>
 <blockquote class="postmatic-elevated">
-  <h4><?php esc_html__e( 'From the Conversation.', 'elevated-comment' ); ?></h4>
+  <h4><?php _e( 'From the Conversation', 'elevated-comments' ); ?></h4>
   <div class="postmatic-elevated-avatar"><?php echo get_avatar($comment->comment_author_email); ?></div>
   <div class="postmatic-elevated-comment"><?php echo $comment->comment_content ?></div>
   <cite>- <a href="<?php echo get_comment_link($comment); ?>"><?php echo $comment->comment_author; ?></a></cite>
