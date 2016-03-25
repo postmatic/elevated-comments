5dd7071c41f3612a1f97078025c79ee4406313e0
Jason Lemieux <jason@gopostmatic.com>
2016-03-25 21:20:35 +0000

    new style and markup

--- assets/templates/elevated-comment.php
+++ assets/templates/elevated-comment.php
@@ -7,9 +7,11 @@
  *  WP_Commment $comment
  */
 ?>
-<blockquote class="postmatic-elevated">
-  <h4><?php _e( 'From the Conversation', 'elevated-comments' ); ?></h4>
+<div class="postmatic-elevated">
+
+  <div class="postmatic-elevated-comment"><p><?php echo $comment->comment_content ?></p></div>
   <div class="postmatic-elevated-avatar"><?php echo get_avatar($comment->comment_author_email); ?></div>
-  <div class="postmatic-elevated-comment"><?php echo $comment->comment_content ?></div>
-  <cite>- <a href="<?php echo get_comment_link($comment); ?>"><?php echo $comment->comment_author; ?></a></cite>
-</blockquote>
+  <div class="postmatic-elevated-author"><?php echo $comment->comment_author; ?></div>
+  <a class="postmatic-elevated-link" href="<?php echo get_comment_link($comment); ?>"><?php _e( 'From the comments', 'elevated-comments' ); ?></a>
+  
+</div>
