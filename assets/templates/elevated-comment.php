b501ca554402b4127d327d13d2833a4cd68e8eee
Jason Lemieux <jason@gopostmatic.com>
2016-04-12 01:38:44 +0000

    some wpautop

--- assets/templates/elevated-comment.php
+++ assets/templates/elevated-comment.php
@@ -9,7 +9,7 @@
 ?>
 <div class="postmatic-elevated">
 
-  <div class="postmatic-elevated-comment"><p><?php echo $comment->comment_content ?></p></div>
+  <div class="postmatic-elevated-comment"><?php echo apply_filters('comment_text', $comment->comment_content); ?></div>
   <div class="postmatic-elevated-avatar"><?php echo get_avatar($comment->comment_author_email); ?></div>
   <div class="postmatic-elevated-author"><?php echo $comment->comment_author; ?></div>
   <a class="postmatic-elevated-link" href="<?php echo get_comment_link($comment); ?>"><?php _e( 'From the comments', 'elevated-comments' ); ?></a>
